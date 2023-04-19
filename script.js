ee701b11c0764887d3c0efcaa57490a55ce76b63
Lopan <116125124+Lopan91@users.noreply.github.com>
2023-04-19 08:04:46 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sfuncao = document.querySelector('#m-funcao')
+const sSalario = document.querySelector('#m-salario')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
