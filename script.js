12e0ecc47b777947d226b60bd05b09c8ac65ec2f
Lopan <116125124+Lopan91@users.noreply.github.com>
2023-04-19 10:27:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,34 +8,26 @@ const btnSalvar = document.querySelector('#btnSalvar')
 let itens
 let id
 
-const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
-const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+function openModal(edit = false, index = 0){
+    modal.classList.add('active')
 
-function loadItens() {
-    itens = getItensBD()
-    tbody.innerHTML = ''
-    itens.forEach((item, index) => {
-        insertItem(item, index)
-    })
-}
+    modal.onclick = e => {
+        if (e.target.className.indexOf('modal-container') !== -1) {
+            modal.classList.remove('active')
+        }
+    }
 
-loadItens()
+    if (edit) {
+        sNome.value = itens[index].nome
+        sfuncao.value = Intns[index].funcao
+        sSalario.value = itens[index].salario
+        id = index  
+    } else {
+        sNome.value = ''
+        sfuncao.value = ''
+        sSalario.value = ''
+    }
 
-function insertItem(item, index) {
-    let tr = document.createElement('tr')
-
-    tr.innerHTML = `
-        <td>${item.nome}</td>
-        <td>${item.funcao}</td>
-        <td>R$ ${item.salario}</td>
-        <td class="acao">
-        <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
-        </td>
-        <td class="acao">
-            <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
-        </td>
-    `
-    tbody.appendChild(tr)
 }
 
 function editItem(index) {
@@ -48,3 +40,57 @@ function deleteItem(index) {
     setItensBD()
     loadItens()
 }
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+        <td>${item.nome}</td>
+        <td>${item.funcao}</td>
+        <td>R$ ${item.salario}</td>
+        <td class="acao">
+            <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+        </td>
+        <td class="acao">
+            <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+        </td>
+    `
+    tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+
+    if (sNome.value == '' || sfuncao.value == '' || sSalario.value == '') {
+        return
+    }
+
+    e.preventDefault();
+
+    if (id !== undefined) {
+        itens[id].nome = sNome.value
+        itens[id].funcao = sfuncao.value
+        itens[id].salario = sSalario.value
+    } else {
+        itens.push({'nome' : sNome.value, 'funcao': sfuncao.value, 'salario': sSalario.value})
+    }
+
+    setItensBD()
+
+    modal.classList.remove('active')
+    loadItens()
+    id = undefined
+}
+
+function loadItens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+        insertItem(item, index)
+    })
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
